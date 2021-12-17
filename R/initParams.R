5568049b47b1603ef3c9c6526ee4a4581c8277ac
beyondpie <panshi.pie@gmail.com>
2021-12-17 13:48:13 -0800

    add part of functions to initialize negative binomial distribution

---
+++ R/initParams.R
@@ -0,0 +1,128 @@
+#' Estimate the normalized mean of a numeric vector on log-level,
+#' which is similar with log(mean(y/s)) (or better),
+#'
+#' NOTE previsous name: init_snb_log_mean
+#' @param y numeric vector, should be not all zeros.
+#' @param s numeric vector, normalizing constant, should no zeros.
+#' @return numeric scalar
+#' @export
+normLogMean <- function(y, s) {
+  if (any(y) < 0) {
+    stop("numeric vector y should be no less than zero.")
+  }
+  if(sum(y) == 0){
+    stop("numeric vector y should have at least one element larger than zero.")
+  }
+  if(any(s <= 0) ) {
+    stop("normalizing constant s should be all larger than zero.")
+  }
+  mu <- log(mean(y)) - log(median(s))
+  invisible(mu)
+}
+
+#' Get the mu (log-level mean) and r (dispersion) in Negative Binomial with scaling.
+#' dispersion is defined as the same as in either in R or Stan.
+#'
+#' NOTE:
+#' From observation, mean can be estimate well, but if it's below 1,
+#' then r tends to be estimated too small.
+#' @param y numeric vector, should be not all zeros.
+#' @param s numeric vector, normalizing constant, should no zeros.
+#' @param min_r numeric scalar, dispersion lower threshold.
+#' @return list of two-named scalar, mu and r
+#' @export
+initNBParam <- function(y, s, min_r) {
+  mu <- normLogMean(y = y, s = s)
+  ## v equals to m + m^2/r
+  v <- var(y)
+  m <- mean(y)
+  ## dispersion
+  r <- ifelse(v > m, m^2 / (v - m), min_r)
+  invisible(list(mu = mu, r = r))
+}
+
+#' Fit mu, mucond, muind for Negative Binomial with scaling.
+#' No more Stan script based.
+#' @param cond vector of integer, start from 1
+#' @param ind vector of integer, start from 1
+#' @return list of four-named element, mu, r, mucond, muind
+#' mu and r are scalars.
+#' mucond is a vector length of number of conditions, usually 2.
+#' muind is a vector length of number of individuals.
+#' @export
+initNBParamWithCondBatch <- function(y, s, cond, ind,
+                                     default_mu = 0.0,
+                                     default_r = 20) {
+  nind <- max(ind)
+  ncond <- max(cond)
+  result <- list(
+    mu = default_mu,
+    r = default_r,
+    mucond = rep(0.0, ncond),
+    muind = rep(0.0, nind)
+  )
+  if (any(y) < 0) {
+    stop("numeric vector y should be no less than zero.")
+  }
+  if(sum(y) == 0){
+    ## if y are all zeros, then we use the default ones.
+    ## TODO: maybe we can set muind as the mean of muind from our model,
+    ## which can be directly estimated by a pure non-mucond effect model.
+    ## for example, we can use random effect model?
+    return(invisible(result))
+  }
+
+  init_mur <- initNBParam(y, s)
+  result$mu <- init_mur$mu
+  result$r <- init_mur$r
+  
+  result$mucond <- vapply(1:ncond, function(i) {
+      ss <- s[cond == i]
+      yy <- y[cond == i]
+      if(sum(yy) == 0) {
+        return(0.0)
+      }
+      t <- normLogMean(y = yy, s = ss)
+      init_mucond <- t - result$mu
+      ## Since fitting is hard when fix r, and limited data
+      ## we directly use the init_mucond
+      invisible(init_mucond)
+    },
+    FUN.VALUE = 0.0
+  )
+  
+  result$muind <- vapply(1:nind, function(i) {
+      yy <- y[ind == i]
+      if (sum(yy) == 0) {
+        return(0.0)
+      }
+      t <- normLogMean(y = yy, s = s[ind == i])
+      return(invisible(t - result$mu - result$mucond[cond[ind == i][1]]))
+    },
+    FUN.VALUE = 0.0
+  )
+  invisible(result)
+}
+
+
+#' @export
+fitGeneGlobalMeanPriorParams <- function(mu) {
+  m <- median(mu)
+  ngene <- length(mu)
+  v <- sum((mu - m)^2) / ngene
+  v <- max(v, self$min_varofmu)
+  ## varofmu prior follows a inv-gamma dist
+  ## est hy based on posterior
+  alpha <- 1.0 + ngene / 2
+  beta <- 1.0 + sum((mu - m)^2) / 2
+  invisible(c(m, v, alpha, beta))
+}
+
+
+
+
+
+
+
+
+
---
+++ R/stanUtils.R
@@ -0,0 +1,116 @@
+#' Compile stan model with cmdstanr
+#'
+#' NOTE previous name: init_stan_model
+#' @param model_path string, stan script
+#' @param use_thread bool/NULL, when reduce_sum or map_rect in script, set it as TRUE.
+#' When running, remember to export STAN_NUM_THREADS=4. Default is NULL.
+#' @param use_mpi bool/NULL, when map_rect in script, set it as TRUE. Deault is NULL.
+#' @param use_opencl bool/NUYLL, enable the OpenCL backend. Deault is NULL.
+#' When set is as TRUE, remember to set STAN_OPENCL=true when compiling.
+#' https://mc-stan.org/docs/2_26/cmdstan-guide/parallelization.html
+#' - The Stan model compiled with STAN_OPENCL can also be supplied the
+#'  OpenCL platform and device IDs of the target device. These IDs
+#'  determine the device on which to run the OpenCL-supported functions
+#'  on. You can list the devices on your system using the clinfo
+#'  program. If the system has one GPU and no OpenCL CPU runtime, the
+#'  platform and device IDs of the GPU are typically 0. In that case you
+#'  can also omit the OpenCL IDs as the default 0 IDs are used in that
+#'  case.
+#'  We supply these IDs when starting the executable as shown below:
+#'  path/to/model sample data file=data.json opencl platform=0 device=1
+#' 
+#' NOTE[BUG]:  use_xxx = NULL when we don't want to use them.
+#' When I set them as FALSE, they still treat them as TRUE.
+#' @return compiled stan model
+#' @export
+compileStanModel <- function(model_path,
+                             use_thread = NULL,
+                             use_mpi = NULL,
+                             use_opencl = NULL) {
+  if(!file.exists(model_path)){
+    stop(paste(model_path, "does not exist."))
+  }
+  r <- suppressMessages(cmdstanr::cmdstan_model(
+    stan_file = model_path,
+    compile = TRUE,
+    quiet = TRUE,
+    pedantic = TRUE,
+    cpp_options = list(stan_threads = use_thread,
+                       stan_map = use_mpi,
+                       stan_opencl = use_opencl),
+    stanc_options = list(),
+    force_recompile = FALSE
+  ))
+  invisible(r)
+}
+
+#' @param nm string, name of variable in stan script
+#' @param n integer, length of the variable.
+#' @param l string, left bracket, default "["
+#' @param r string, right brackt, default "]"
+#' @return vector of string
+#' @export
+getNameOfVectorFromCmdstanr <- function(nm = "MuInd", n = 10L, l = "[", r = "]") {
+  invisible(vapply(seq_len(n), function(i) {
+    paste0(nm, ls, i, rs)
+  }, FUN.VALUE = "MuInd[1]"))
+}
+
+
+#' @param nm string, name of variable in stan script
+#' @param nr integer, nrow of the variable.
+#' @param nc integer, ncolof the variable.
+#' @param l string, left bracket, default "["
+#' @param r string, right brackt, default "]"
+#' @return vector of string
+#' @export
+getNameOfMatrixFromCmdstanr <- function(nm = "MuInd", nr, nc, l = "[". r = "]") {
+  r <- rep("", nr * nc)
+  for (i in 1:nr) {
+    for (j in 1:nc) {
+      r[nc * (i - 1) + j] <- paste0(nm, ls, i, ",", j, rs)
+    }
+  }
+  invisible(r)
+}
+
+#' Replicate a vector in row-wise for n times.
+#'
+#' NOTE previous name: rep_row
+#' @param x vector of numeric
+#' @param n integer, times of repeat in row-wise
+#' @return matrix
+#' @export  
+repVecRowise <- function(x, n) {
+  matrix(rep(x, each = n), nrow = n, byrow = FALSE)
+}
+
+#' Replicate a vector in column-wise for n times.
+#'
+#' NOTE previous name: rep_col
+#' @param x vector of numeric
+#' @param n integer, times of repeat in column-wise
+#' @return matrix
+#' @export
+repVecColwise <- function(x, n) {
+  matrix(rep(x, each = n), ncol = n, byrow = TRUE)
+}
+
+#' Split matrix columns into a matrix, which is row-major order
+#' @param mat matrix, size of nsample by (second_dim * third_dim)
+#' @param secondim integer, secondim we want
+#' @param secondimNms vector of string, names for secondim.
+#' @return three-dim array
+#' @export
+split_matrix_col <- function(mat, secondim, secondimNms = NULL) {
+  t <- vapply(seq_len(nrow(mat)), function(i) {
+    ## byrow = TRUE, means we order the elements row by row.
+    ## so each time we use ncol element to fill a row.
+    matrix(mat[i, ], nrow = secondim, byrow = TRUE)
+  }, FUN.VALUE = matrix(mat[1, ], nrow = secondim, byrow = TRUE))
+  r <- aperm(t, c(3, 1, 2))
+  if (!is.null(secondimNms)) {
+    dimnames(r)[[2]] <- secondimNms
+  }
+  return(invisible(r))
+}
